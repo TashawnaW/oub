278df33bda14eeea7aa296b9a587760196ebce14
TashawnaW <tashawnawells@gmail.com>
2024-09-15 16:51:30 -0400

    Declared inputs in array

---
+++ script.js
@@ -0,0 +1,16 @@
+// Part 1: Thinking Functionally 
+
+
+
+
+
+
+// Declar variables in the array
+const array= [3,4,5,6,7,8]
+
+// Return Sum of Array
+
+sum_Array(arr); 
+return sum (arr); 
+console.log(sum_Array)
+
